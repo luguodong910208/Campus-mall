a76b56cc6a7cbd23dfb3aa6c6def88a0089fca11
lu <992090718@qq.com>
2018-09-10 20:21:24 +0800

    new project
    using SSM and SpringBoot to make an campusmall

---
+++ o2o/src/main/java/com/imooc/o2o/entity/HeadLine.java
@@ -0,0 +1,10 @@
+package com.imooc.o2o.entity;
+
+public class HeadLine {
+
+	private Long lineId;
+	private String lineName;
+	private String lineLink;
+	private String lineImg;
+	private Integer priority;
+}
