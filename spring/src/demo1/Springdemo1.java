ff67e5dd66dcbbe61e7c766d1509cb7ecb0a337c
lu <992090718@qq.com>
2018-09-14 23:46:53 +0800

    add

--- spring/src/demo1/Springdemo1.java
+++ spring/src/demo1/Springdemo1.java
@@ -16,4 +16,18 @@ public class Springdemo1 {
 		Car car = (Car) applicationContext.getBean("car");
 		System.out.println(car);
 	}
+	
+	@Test
+	public void demo2() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Car2 car2 = (Car2) applicationContext.getBean("car2");
+		System.out.println(car2);
+	}
+	
+	@Test
+	public void demo3() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee employee = (Employee) applicationContext.getBean("employee");
+		System.out.println(employee);
+	}
 }
