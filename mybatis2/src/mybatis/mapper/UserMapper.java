2cec472c3a61ab927435877108be602221f27dd6
lu <992090718@qq.com>
2018-09-25 21:45:58 +0800

    add

---
+++ mybatis2/src/mybatis/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package mybatis.mapper;
+
+import java.util.List;
+
+import mybatis.pojo.QueryVo;
+import mybatis.pojo.User;
+
+public interface UserMapper {
+	//遵循四大原则
+	//接口方法名 == User.xml中id名
+	//返回值类型与Mapper.xml文件中返回值类型一致
+	//方法的入参类型与Mapper.xml入参类型一直
+	//命名空间绑定此接口
+	public User findUserById(Integer id);
+	public List<User> findUserByQueryVo(QueryVo vo);
+	public Integer userCount();
+}
---
+++ mybatis2/src/mybatis/pojo/QueryVo.java
@@ -0,0 +1,14 @@
+package mybatis.pojo;
+
+import java.io.Serializable;
+
+public class QueryVo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private User user;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}	
+}
